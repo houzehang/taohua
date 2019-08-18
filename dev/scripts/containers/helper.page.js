793ad0a3b8dc6235fa288bebfa42f697627b281a
houzehang <2210036910@qq.com>
2019-08-18 22:43:00 +0800

    bugfix

--- dev/scripts/containers/helper.page.js
+++ dev/scripts/containers/helper.page.js
@@ -1,69 +1,93 @@
 
 require('../../less/helper.less')
 import React from 'react';
-import {
-	HashRouter as Router,
-	Route
-} from 'react-router-dom'
-import { connect } from 'react-redux'
-import Login from './login.page'
-import MainTeacherPage from './main.teacher.page'
-import MainStudentPage from './main.student.page'
-import Dialog from './dialog'
-import { alert, confirm, onNetStatusBad, onNetStatusGood } from '../actions'
-const NetDetector = require("../netdetector")
-const context = require("../context")
-import * as types from '../constants/ActionTypes'
 
-class Entry extends React.Component {
+class Helper extends React.Component {
 	constructor(props) {
 		super(props)
 	}
 	componentDidMount() {
-		this.__start_detector()
 	}
 
 	componentWillUnmount() {
-		this.$detector.unload()
-	}
-
-	__start_detector() {
-		let detector = new NetDetector
-		context.detector = detector
-		context.detector.waring_threshold = 3;
 	}
 	
 	render() {
-		const { dialog, account } = this.props
 		return  <div className="helper-page">
            <div className="helper-panel">
                 <div className='nav-bar'>
-                    <div className='shell-item name'>脚本名称</div>
-                    <div className='shell-item path'>脚本路径</div>
-                    <div className='shell-item state'>当前状态</div>
-                    <div className='shell-item controll'>控制</div>
+                    <div className='nav-item name'>脚本名称</div>
+                    <div className='nav-item path'>脚本路径</div>
+                    <div className='nav-item state'>当前状态</div>
+                    <div className='nav-item controll'>选项</div>
+                    <div className='nav-item hotkey'>快捷键</div>
                 </div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
+				<div className='shell-line'>
+					<input className='shell-item name' placeholder="kcpc-watch"/>
+					<div className='shell-item path'>../youshi/kecheng-pc</div>
+					<div className='shell-item state'>已开启</div>
+					<div className='shell-item controll'>
+
+					</div>
+					<div className='shell-item hotkey'>F1</div>
+				</div>
            </div>
         </div>
 	}
 }
 
-const mapStateToProps = (state, ownProps) => {
-	return {
-		account 	: state.login.account,
-		dialog  	: state.dialog
-	}
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-	return {
-		confirm : (data) 	=> dispatch(confirm(data)),
-		onNetStatusBad: () 	=> dispatch(onNetStatusBad()),
-		onNetStatusGood: () => dispatch(onNetStatusGood())
-	}
-}
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Entry)
+export default Helper
